6cb0e01a306de5bdfaea1088bc91adeeba7af6aa
Tanmay Baweja <31308303+Tanmay98@users.noreply.github.com>
2020-07-19 00:22:26 +0530

    Add files via upload

--- homework.py
+++ homework.py
@@ -29,3 +29,29 @@ def sw_vehicle_search(cargo_capacity, max_speed, cost):
         
         url = data["next"]
     return output
+
+
+def starship_piloted_species(starship: str):
+	#this is similar to above, follow the same steps like above
+
+def cat_language(breed, language):
+    url = 'https://api.thecatapi.com/v1/breeds/search?q='+str(breed)
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+    data = resp.json()
+    country_origin = data[0]["origin"]
+    
+    url2 = 'https://restcountries.eu/rest/v2/name/'+str(country_origin)
+    resp2 = requests.get(url2)
+    if resp2.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    
+    data2 = resp2.json()
+    lang_code = data2[0]["languages"][0]["iso639_1"]
+    
+    if language == lang_code:
+        return True
+    else: return False
+
